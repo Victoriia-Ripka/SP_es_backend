498b8f039c37bb09ffdc9bbddb3c381ab4f15b32
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-04-27 01:11:41 +0200

    .

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const corsOptions = {
 };
 
 app.use(logger(formatsLogger));
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
