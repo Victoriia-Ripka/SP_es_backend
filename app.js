a6cab13775193b7d8981e6d431a7846cb35c8333
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-04-27 19:41:46 +0200

    fix cors errors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const allowedOrigins = ['https://victoriia-ripka.github.io'];
+const allowedOrigins = ['https://victoriia-ripka.github.io', 'http://localhost:3000'];
 
 const corsOptions = {
     origin: (origin, callback) => {
@@ -20,10 +20,24 @@ const corsOptions = {
 };
 
 app.use(logger(formatsLogger));
-app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://victoriia-ripka.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 app.use("/assistant", assistantRoute);
 
