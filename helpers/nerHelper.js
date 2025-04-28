39ab32a0d9ce5c97dc1b633b0910661762af0868
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-04-28 12:24:02 +0200

    optimization helpers ner and intent

--- helpers/nerHelper.js
+++ helpers/nerHelper.js
@@ -3,28 +3,25 @@ import path from 'path';
 import yaml from 'js-yaml';
 import { processInputWithGPT } from './openAIHelper.js';
 
-const nerExamples = yaml.load(fs.readFileSync(path.resolve('./config/ner_examples.yml'), 'utf8'));
+const nerExamplesFile = yaml.load(fs.readFileSync(path.resolve('./config/ner_examples.yml'), 'utf8'));
 
-function buildNERPrompt(userInput, examples) {
+function buildNERPrompt(examples) {
     const examplesText = examples.map(ex => `Text: ${ex.text}\nEntities: ${JSON.stringify(ex.entities)}`).join('\n\n');
     return `
-        Виділи іменовані сутності з тексту.
-
+        Ти допомагаєш витягати сутності з тексту. Намагайся виявити всі можливі сутності згідно до прикладів.
         Приклади:
         ${examplesText}
 
-        Текст для обробки:
-        ${userInput}
-        Формат відповіді: JSON масив з об'єктами { "text": "...", "label": "..." }
+        Формат відповіді: JSON масив з об'єктами { "text": "...", "label": "..." }. 
+        Якщо сутностей не виявлено - поверни пустий масив [] і більше нічого. 
         Повертай лише дійсний масив сутностей JSON, більше нічого. Не включай жодного тексту, пояснень чи форматування.
     `;
 };
 
 async function extractEntitiesFromText(text) {
-    const nerPrompt = buildNERPrompt(text, nerExamples);
-    const nerContent = 'Ти допомагаєш витягати сутності з тексту. Якщо сутностей не виявлено - поверни пустий масив [] і більше нічого. Намагайся виявити всі можливі сутності згідно до файлу з прикладами';
-    const nerEntities = JSON.parse(await processInputWithGPT(nerContent, nerPrompt));
-
+    const context = buildNERPrompt(nerExamplesFile);
+    const nerEntities = JSON.parse(await processInputWithGPT(context, text));
+    
     console.log('[INFO NER]', nerEntities);
     return nerEntities;
 }
--- helpers/textcatHelper.js
+++ helpers/textcatHelper.js
@@ -2,26 +2,22 @@ import fs from 'fs';
 import path from 'path';
 import { processInputWithGPT } from './openAIHelper.js';
 
-const textcatExamples = JSON.parse(fs.readFileSync(path.resolve('./config/textcat_examples.json'), 'utf8'));
+const textcatExamplesFile = JSON.parse(fs.readFileSync(path.resolve('./config/textcat_examples.json'), 'utf8'));
 
-function buildTextcatPrompt(userInput, examples) {
+function buildTextcatPrompt(examples) {
     const examplesText = examples.map(ex => `Text: ${ex.text}\nIntent: ${ex.label}`).join('\n\n');
     return `
-        Визнач намір користувача.
-
+        Ти класифікуєш наміри користувача. Визнач намір користувача.
         Приклади:
         ${examplesText}
 
-        Текст для класифікації:
-        ${userInput}
-        Формат відповіді: тільки мітка наміру (наприклад: "орендувати", "прощання", "інформація")
+        Формат відповіді: тільки мітка наміру (наприклад: "визначити місце монтажу", "привітання", "інформація")
     `;
 };
 
 async function extractIntentFromText(userInput) {
-    const textcatPrompt = buildTextcatPrompt(userInput, textcatExamples);
-    const textcatContent = 'Ти класифікуєш наміри користувача.';
-    const intent = await processInputWithGPT(textcatContent, textcatPrompt);
+    const context = buildTextcatPrompt(textcatExamplesFile);
+    const intent = await processInputWithGPT(context, userInput);
 
     console.log('[INFO Intent]', intent);
     return intent;
