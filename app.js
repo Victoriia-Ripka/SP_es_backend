191b536c03d3acc1e9db77d3befbb30854f800f6
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-05-09 09:25:54 +0200

    cors

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const allowedOrigins = ['https://victoriia-ripka.github.io/', 'https://victoriia
 const corsOptions = {
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }
 
 app.use(cors(corsOptions));
